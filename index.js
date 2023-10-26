e4174c04bf88ff98bbbd0f7e44e4219cb81954f7
caicangcua <36462470+caicangcua@users.noreply.github.com>
2023-10-27 06:57:04 +0700

    v2

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
+const PORT = process.env.PORT || 3000
 
-const port = process.env.PORT || 3000
+const dgram = require('dgram');
+const server = dgram.createSocket('udp4');
 
-const datagram = require("dgram");
-const socket = datagram.createSocket("udp4");
+const HOST = '0.0.0.0';
+const PORT = 30000;
 
-//listen incomming messages
-socket.on("message", (msg, receInfo) => {
-    //display msg and sender information ip:port
-    console.log(`I Have Already Received: ${msg} from ${receInfo.address}:${receInfo.port}`);
+server.on('error', (err) => {
+    console.log(`server error:\n${err.stack}`);
+    server.close();
 });
-socket.bind(port);
+
+server.on('message', (msg, rinfo) => {
+    console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
+});
+
+server.on('listening', () => {
+    const address = server.address();
+    console.log(`server listening ${address.address}:${address.port}`);
+});
+
+server.bind({
+    address: HOST,
+    port: PORT,
+    exclusive: true
+});
